d40c6a09d299f3d82aa37c574fd42513ea57e0eb
Jeremy Duong <jeremibq@gmail.com>
2021-11-05 12:21:56 -0600

    Handled health bar in a way ready for change

--- astroid/cast/mothership.py
+++ astroid/cast/mothership.py
@@ -1,10 +1,10 @@
 from astroid.cast.HasLifeActor import HasLifeActor
 
-class Astroid(HasLifeActor):
+class MotherShip(HasLifeActor):
     def __init__(self, path: str,
                 health_bar_y_offset: int,
                 health_bar_height: int = 5,
-
+                
                 width: int = 0, 
                 height: int = 0, 
                 
@@ -17,14 +17,13 @@ class Astroid(HasLifeActor):
                 rotation: float = 0, 
                 rotation_vel: float = 0,
                 
-                points: int = 0,
-                max_hp: int = 0,
+                max_hp: int = 100,
                 show_text_health : bool = False):
 
         super().__init__(path,
-                        health_bar_y_offset=health_bar_y_offset,
-                        health_bar_height=health_bar_height,
-                        
+                        health_bar_y_offset = health_bar_y_offset,
+                        health_bar_height = health_bar_height,
+
                         width=width, 
                         height=height, 
                         
@@ -36,13 +35,6 @@ class Astroid(HasLifeActor):
                         
                         rotation=rotation, 
                         rotation_vel=rotation_vel,
+                        
                         max_hp=max_hp,
                         show_text_health=show_text_health)
-
-        self._points = points
-
-    def set_points(self, points):
-        self._points = points
-    
-    def get_point(self):
-        return self._points
