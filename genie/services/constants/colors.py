7ab2964fd19fc445c527c06a97cf51fdc040526e
Jeremy Duong <jeremibq@gmail.com>
2021-11-22 19:55:03 -0700

    fixed colors commit mistake of last commit

--- genie/services/constants/colors.py
+++ genie/services/constants/colors.py
@@ -8,12 +8,12 @@
     Opacity goes from 0 to 255, with 0 being completely vanished and
         255 being completely not see-through-able
 """
-BLACK = (0, 0, 0)
-GRAY = (127, 127, 127)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0, 255)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-CYAN = (0, 255, 255)
-MAGENTA = (255, 0, 255)
+BLACK = (0, 0, 0, 255)
+GRAY = (127, 127, 127, 255)
+WHITE = (255, 255, 255, 255)
+RED = (255, 0, 0, 255, 255)
+GREEN = (0, 255, 0, 255)
+BLUE = (0, 0, 255, 255)
+YELLOW = (255, 255, 0, 255)
+CYAN = (0, 255, 255, 255)
+MAGENTA = (255, 0, 255, 255)
